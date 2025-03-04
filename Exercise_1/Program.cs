31c6312674c3ffd5c1902643a5e9102f6860d118
Yusuf Taral <yutaral99@gmail.com>
2025-03-04 11:56:29 +0200

    Add project files.

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,124 @@
+﻿class Program
+{
+    private static readonly char CURLY_OPENING_BRACKET = '{';
+    private static readonly char SQUARE_OPENING_BRACKET = '[';
+    private static readonly char ROUND_OPENING_BRACKET = '(';
+
+    private static readonly char CURLY_CLOSING_BRACKET = '}';
+    private static readonly char SQUARE_CLOSING_BRACKET = ']';
+    private static readonly char ROUND_CLOSING_BRACKET = ')';
+
+    private static void Main()
+    {
+        string[] exp = { "[()]{}{[()()]()}", "[(])"};
+
+        for (int i = 0; i < exp.Length; i ++)
+        {
+
+            if (BalancedBrackets(exp[i]))
+            {
+                Console.WriteLine("Balanced");
+            }
+            else
+            {
+                Console.WriteLine("Not Balanced");
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Check whether the string contains balanced brackets
+    /// </summary>
+    /// <param name="exp">
+    ///     The string to check
+    /// </param>
+    /// <returns>
+    ///     True if the brackets are balanced, false otherwise
+    /// </returns>
+    private static bool BalancedBrackets(string exp)
+    {
+        if (string.IsNullOrEmpty(exp))
+        {
+            // Empty string
+            return false;
+        }
+
+        Stack<char> stack = new();
+
+        foreach (char symbol in exp)
+        {
+            if (IsOpening(symbol))
+            {
+                stack.Push(symbol);
+
+            } 
+            else if (IsClosing(symbol))
+            {
+                if (stack.Count == 0)
+                {
+                    // There is closing bracket, but no opening
+                    return false;
+                }
+
+                var lastBracket = stack.Pop();
+
+                if (!AreMatchingBrackets(lastBracket, symbol))
+                {
+                    // The last bracket in the stack and the current symbol
+                    // are not matching brackets 
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    /// <summary>
+    ///     Check whether the symbol is opening bracket
+    /// </summary>
+    /// <param name="symbol">
+    ///     The symbol to check
+    /// </param>
+    /// <returns>
+    ///     True if the symbol is opening bracket, false otherwise
+    /// </returns>
+    private static bool IsOpening(char symbol) {
+        return symbol == CURLY_OPENING_BRACKET || symbol == SQUARE_OPENING_BRACKET || symbol == ROUND_OPENING_BRACKET;
+    }
+
+    /// <summary>
+    ///     Check whether the symbol is closing bracket
+    /// </summary>
+    /// <param name="symbol">
+    ///     The symbol to check
+    /// </param>
+    /// <returns>
+    ///     True if the symbol is closing bracket, false otherwise
+    /// </returns>
+    private static bool IsClosing(char symbol)
+    {
+        return symbol == CURLY_CLOSING_BRACKET || symbol == SQUARE_CLOSING_BRACKET || symbol == ROUND_CLOSING_BRACKET;
+    }
+
+    /// <summary>
+    ///     Check whether the symbols are matching brackets(same type) - e.g '{' and '}'
+    /// </summary>
+    /// <param name="first">
+    ///     The first symbol 
+    /// </param>
+    /// <param name="second">
+    ///     The second symbol 
+    /// </param>
+    /// <returns>
+    ///     True if the symbols are matching brackets, false otherwise
+    /// </returns>
+    private static bool AreMatchingBrackets(char first, char second)
+    {
+        return (first == CURLY_OPENING_BRACKET && second == CURLY_CLOSING_BRACKET) ||
+                (first == SQUARE_OPENING_BRACKET && second == SQUARE_CLOSING_BRACKET) ||
+                (first == ROUND_OPENING_BRACKET && second == ROUND_CLOSING_BRACKET);
+
+    }
+
+}
---
+++ Exercise_2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    private static void Main()
+    {
+        int[][] numbers = [[2, 2, 1], [4, 1, 2, 1, 2], [1]];
+
+        foreach (int[] array in numbers)
+        {
+            PrintNonRepeatingNumber(array);
+        }
+    }
+
+    /// <summary>
+    ///     Search for a non repeating number in the array
+    ///     and print it if there is
+    /// </summary>
+    /// <param name="numbers">
+    ///     The array of numbers
+    /// </param>
+    private static void PrintNonRepeatingNumber(int[] numbers)
+    {
+        HashSet<int> checkedNumbers = [];
+
+        foreach (int n in numbers)
+        {
+            if (checkedNumbers.Contains(n))
+            {
+                // If hash set already contains this number, remove it
+                checkedNumbers.Remove(n);
+            } else
+            {
+                checkedNumbers.Add(n);
+            }
+        }
+
+        // Given the task's requirement that there will always be one non repeating number,
+        // just print the first element of the hash set
+        Console.WriteLine(checkedNumbers.First());
+    }
+}
